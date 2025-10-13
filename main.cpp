70691ce4c6bf1f6fd341dd3d57474e9e05b6b297
Paz <pazgonzalezgigena@gmail.com>
2025-10-12 21:14:10 -0300

    D

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-#include "headers/products.h"
+#include "headers/products/products.h"
 int main() {
     cout << "Hola Maxi desde C++!" << endl;
     return 0;
